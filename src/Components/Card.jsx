4666d68dd0cecaee5bc1eb4dd5b82861c2e5e64f
Angelica Barbieri Lirio <angelicablirio@gmail.com>
2022-12-06 20:48:00 -0300

    created requisitions card and function for favorite

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = (props) => {
 
   return (
     <>
@@ -13,11 +13,13 @@ const Card = () => {
           alt="doctor placeholder"
         />
         <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
           <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
+            <h5 className={`card-title ${styles.title}`}>{props.item.nome}</h5>
           </a>
+        </div>
+        <div>
+          <p>{props.item.usuario.username}</p>
+          <button className={styles.favButton} onClick={() => props.handleToggleFavorite()}>{props.checkIfIsFavorite()}</button>
         </div>
       </div>
     </>
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,19 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
 
 const Home = () => {
 
+  const [dentistas, setDentistas] = useState([]);
+  const [favorite, setFavorite] = useState(false);
+
   useEffect(() => {
-    //Nesse useEffect, deverá ser obtido todos os dentistas da API
-    //Armazena-los em um estado para posteriormente fazer um map
-    //Usando o componente <Card />
+    fetch(`https://dhodonto.ctdprojetos.com.br/dentista`).then(
+      response => {
+        response.json().then(
+          dentistArray => {
+            setDentistas(dentistArray)
+          }
+        )
+      }
+    )
   }, []);
+
+  const checkIfIsFavorite = () => {
+    if(favorite){
+      return "❌ Unfavorite your Doc"
+    }else{
+      return "⭐ Favorite your Doc"
+      
+    } 
+  }
+
+  const handleToggleFavorite = () => {
+    if(favorite){
+      alert("Dentist removed successfully")
+      setFavorite(false)
+    }else{
+      alert("Dentist added successfully")
+      setFavorite(true)
+    }
+  };
 
   return (
     <>
       <h1>Home</h1>
       <div className="card-grid container">
-        <Card />
+        {dentistas.map((data, index) => (
+          <Card 
+            key={index} 
+            item={data}
+            handleToggleFavorite={handleToggleFavorite}
+            checkIfIsFavorite={checkIfIsFavorite}
+          />
+        )  
+        )}
       </div>
     </>
   );
